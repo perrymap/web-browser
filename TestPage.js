5a70ecf65825ace9e23769ac6ecc8e2163047393
mogulders <mogulders@gmail.com>
2020-10-12 00:35:21 -0500

    have created a google link open email and phone number link buttons

--- TestPage.js
+++ TestPage.js
@@ -5,12 +5,15 @@ import {
   Image,
   StyleSheet,
   TouchableHighlight,
-  Button
+  Button,
+  Linking
 } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import * as WebBrowser from "expo-web-browser";
+import * as MailComposer from "expo-mail-composer";
 import photo from "./photo.jpg";
 
+//This is from the documents it handles the opening of the weh browser Replacing the url is the what url it will take you to
 const TestPage = () => {
   handlePressButtonAsync = async () => {
     let result = await WebBrowser.openBrowserAsync("https://expo.io");
@@ -27,9 +30,28 @@ const TestPage = () => {
           Ultramar ferry terminal. The average trip takes about 35 minutes, but
           can take longer depending upon traffic.{"\n"}
           {"\t"}There are four transportation options available in a variety of
-          price ranges. GOOGLELINKHERE
+          price ranges.
         </Text>
-        <Button title="Google Link" onPress={this.handlePressButtonAsync} />
+        <TouchableOpacity onPress={this.handlePressButtonAsync}>
+          <Text style={styles.googleLink}>Google Link</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() =>
+            MailComposer.composeAsync({ recipients: ["perrymex@gmail.com"] })
+          }
+        >
+          <Text style={styles.googleLink}>Open Email</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL("tel:8777111223");
+          }}
+        >
+          <Text style={styles.googleLink}>Perry Phone Number</Text>
+        </TouchableOpacity>
+
+        {/* You need to make a button for the action to be set off with. You/We can style the button howecer you want  */}
+
         <Image style={styles.imageInline} source={photo} />
 
         <Text style={styles.subhead}>Airport to ferry terminal</Text>
@@ -82,7 +104,8 @@ const styles = StyleSheet.create({
     maxHeight: 250,
     borderWidth: 3,
     borderColor: "blue"
-  }
+  },
+  googleLink: { fontSize: 20 }
 });
 
 export default TestPage;
